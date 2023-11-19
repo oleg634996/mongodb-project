26326cabbdcb52e913537392e499ec87ae5c7480
oleg634996 <olegkolodiy22@gmail.com>
2023-11-19 18:30:18 +0200

    dbsfbhjbfdsjf

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const mongoose = require("mongoose")
-console.log(process.env);
+const mongoose =require("mongoose")
 
-const {DB_HOST}=process.env
+const DB_HOST =
+  "mongodb+srv://diving:SFKjiTZQEvWDWRSo@cluster0.59shgmv.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST).then(() => {
+    console.log("OK oleg")
+})
+
+
+// const mongoose = require("mongoose")
+// const dotenv =require("dotenv")
+// dotenv.config()
+// const {}=process.env
 
 
   
-mongoose.connect(DB_HOST)
-    .then(() =>  console.log("conect ok"))
-    .catch(err => console.log(err.massage));
+// mongoose.connect(DB_HOST)
+//     .then(() =>  console.log("conect ok"))
+//     .catch(err => console.log(err.massage));
 
     
